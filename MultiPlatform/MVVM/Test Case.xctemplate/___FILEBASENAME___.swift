670438d2bb48a3f9a7d7af4b0cf67de25d07971c
Yuta Shoji <yuta_shoji@mail.toyota.co.jp>
2023-10-27 20:56:47 +0900

    initial commit
    Co-authored-by: Shion Koga <shion_koga@mail.toyota.co.jp>

---
+++ MultiPlatform/MVVM/Test Case.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,15 @@
+@testable import MonoenBuddy
+import XCTest
+import Nimble
+
+class ___FILEBASENAME___: XCTestCase {
+	func test_() {
+
+	}
+}
+
+extension ___FILEBASENAME___ {
+	func build() {
+		
+	}
+}
---
+++ MultiPlatform/MVVM/View Model.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+extension ___FILEBASENAME___ {
+	class ViewModel {
+
+	}
+}
